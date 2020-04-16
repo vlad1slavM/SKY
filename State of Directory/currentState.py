09e19600effb00893fe5302d539c974612312409
vlad1slavM <46625953+vlad1slavM@users.noreply.github.com>
2020-04-16 21:13:42 +0500

    StateDir
    first version of func

--- State of Directory/currentState.py
+++ State of Directory/currentState.py
@@ -1,1 +1,17 @@
-pass
+import os
+list_of_files = os.listdir(f'D:\\test')
+
+def writelogs():
+    log = open('currentState.log', 'w')
+    for el in list_of_files:
+        log.write(el + '\n')
+
+    log.close()
+
+print('You want save current state YES/NO')
+
+messadge = input()
+if messadge == 'YES':
+    writelogs()
+else:
+    print('OKEY')
